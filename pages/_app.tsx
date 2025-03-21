15f0637973715f16aec14cacbf3624ea6251bdc1
David Baiye <ozerlihashem@gmail.com>
2025-03-21 11:26:14 +0100

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Shared/Layout";
 import "@/styles/globals.css";
 import { NextSeo } from "next-seo";
+import { Analytics } from "@vercel/analytics/next";
 
 import type { AppProps } from "next/app";
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         description="Portfolio website of the young software engineer "
       />
       <Layout>
+        <Analytics />
         <Component {...pageProps} />
       </Layout>
     </>
